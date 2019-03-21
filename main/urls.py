a1a859c2c0e80226f36a6c6ac3d1de5f8add2e15
pixlmaster <akash.tiwari21aug@gmail.com>
2019-03-22 00:07:10 +0530

    added registration portal

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 
 from . import views
 
-
+app_name="main"
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('login/' ,views.login, name='signin' ),
-    path('signup/' ,views.signup, name= 'signup'),
+    path('login/' ,views.log_in, name='signin' ),
+    path('signup/' ,views.sign_up, name= 'signup'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,22 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import foodmania
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 
 def homepage(request):
 	return render(request=request,
 				  template_name="main/home.html"
 		)
 
-def login(request):
+def log_in(request):
 	return render(
 		request=request,
 		template_name='main/log-in.html'
 		)
 
 
-def signup(request):
+def sign_up(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user=form.save()
+			username = form.cleaned_data.get('username')
+			login(request, user)
+			return redirect("main:homepage")
+		else:
+			for msg in form.error_messages:
+				print(form.error_messages[msg])
+			return render(request = request,template_name = "main/sign-up.html", context={"form":form})
+
+	form = UserCreationForm
 	return render(
 		request=request,
 		template_name='main/sign-up.html',
-		context={"details": foodmania.objects.all}
+		context={"form": form}
 		)
