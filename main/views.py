ea2ec9b1880c8ae1618392666c6b9dda907d3b61
pixlmaster <akash.tiwari21aug@gmail.com>
2019-03-23 21:36:08 +0530

    fixed log-in bug

--- main/views.py
+++ main/views.py
@@ -38,20 +38,21 @@ def logout_request(request):
 	return redirect("main:homepage") 
 
 def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				messages.info(request, f"you are now logged in as {username}")
-				return redirect("main:homepage")
-			else:
-				messages.error(request, "Invalid username or password")
-		else:
-			messages.error(request, "Invalid username or password")
-	form = AuthenticationForm()
-	return render(request = request,
-                  template_name = "main/login.html",
-                  context={"form":form})
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "main/login.html",
+                    context={"form":form})
