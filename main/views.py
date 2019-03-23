96493837bb62b602fe20a76c1395fe4c29828d55
pixlmaster <akash.tiwari21aug@gmail.com>
2019-03-23 20:17:58 +0530

    added pop-ups

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import foodmania
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 def homepage(request):
 	return render(request=request,
@@ -22,11 +23,13 @@ def sign_up(request):
 		if form.is_valid():
 			user=form.save()
 			username = form.cleaned_data.get('username')
+			messages.success(request, f"New Account Created: {username}")
+			messages.info(request, f"you are now logged in as {username}")
 			login(request, user)
 			return redirect("main:homepage")
 		else:
 			for msg in form.error_messages:
-				print(form.error_messages[msg])
+				messages.error(request,f"{msg}: {form.error_messages[msg]}")
 			return render(request = request,template_name = "main/sign-up.html", context={"form":form})
 
 	form = UserCreationForm
