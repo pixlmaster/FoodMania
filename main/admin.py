809d7fcecd0bb5b99992f13e6c0cd051e6bf02ad
pixlmaster <akash.tiwari21aug@gmail.com>
2019-04-05 09:52:42 +0530

    added new models

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import Restaurant, Food, Menu
 
 # Register your models here.
 
+admin.site.register(Menu)
+admin.site.register(Food)
 admin.site.register(Restaurant)
--- main/models.py
+++ main/models.py
@@ -6,9 +6,28 @@ class Restaurant(models.Model):
     Restaurant_email = models.CharField(max_length=200, default="null"	)
     Restaurant_Description = models.CharField(max_length=200, default="none")
     Restaurant_image = models.ImageField(upload_to="images/", default="no image")
-    
+ 
+    class Meta:
+        verbose_name_plural = "Restaurants"   
 
     def __str__(self):
         return self.Restaurant_Name
 
-    
+
+class Menu(models.Model):
+	Menu_Title = models.CharField(max_length=200, default='not set')
+	Menu_categlog = models.ForeignKey(Restaurant, default=1, verbose_name="Restaurants", on_delete=models.SET_DEFAULT)
+	
+	class Meta:
+		verbose_name_plural = "Menus" 
+	def __str__(self):
+		return self.Menu_Title
+
+
+class Food(models.Model):
+	Food_name = models.CharField(max_length=200, default="none");
+	Food_price = models.IntegerField()
+	Menu_items = models.ForeignKey(Menu, default=1, verbose_name="Menus", on_delete=models.SET_DEFAULT)
+	Food_slug = models.CharField(max_length=200, default=1)
+	def __str__(self):
+		return self.Food_name
